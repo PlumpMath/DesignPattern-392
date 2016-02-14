f0428a9d2932173522392663e764eb6427bf2097
simidaxu <callmelaoxu@gmail.com>
2016-02-14 14:35:52 +0800

    装饰者模式

---
+++ src/decorate/Client.java
@@ -0,0 +1,16 @@
+package decorate;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Beverage beverage  = new Espresso();
+		System.out.println(beverage.getDescription() + " $" + beverage.cost());
+		
+		Beverage beverage2 = new HouseBlend();
+		beverage2 = new Mocha(beverage2);
+		System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
+
+	}
+
+}
---
+++ src/decorate/HouseBlend.java
@@ -0,0 +1,16 @@
+package decorate;
+
+public class HouseBlend extends Beverage {
+	
+	public HouseBlend() {
+		// TODO Auto-generated constructor stub
+		description = "HouseBlend";
+	}
+
+	@Override
+	public double cost() {
+		// TODO Auto-generated method stub
+		return 0.89;
+	}
+
+}
